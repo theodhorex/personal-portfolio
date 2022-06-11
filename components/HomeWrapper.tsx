2cd54db6ac268ec9e2502741231cac9472c63361
theodhorex <theodhore76@gmail.com>
2022-06-12 02:04:56 +0800

    small fixes (#4)
    * small fixes
    
    * enable giscus
    
    * post simple comments

--- components/HomeWrapper.tsx
+++ components/HomeWrapper.tsx
@@ -1,22 +1,20 @@
 import { ReactNode } from 'react'
 import Footer from './Footer'
-import { Header } from './Header'
 import SectionContainer from './SectionContainer'
 
 interface Props {
   children: ReactNode
 }
 
-const LayoutWrapper = ({ children }: Props) => {
+const HomeWrapper = ({ children }: Props) => {
   return (
     <SectionContainer>
-      <Header />
-      <div className="flex flex-col justify-between h-screen">
-        <main className="mb-auto">{children}</main>
+      <div className="flex flex-col">
+        <main>{children}</main>
         <Footer />
       </div>
     </SectionContainer>
   )
 }
 
-export default LayoutWrapper
+export default HomeWrapper
